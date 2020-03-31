27044834862f3e61fcc79c6c56afba476c5e7dd2
Michael Wells <wow@duhmarket.com>
2020-03-31 07:54:08 -0400

    Add Main Screen

---
+++ AwesomeNote/Controller/Main+ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Main+ViewController.swift
+//  AwesomeNote
+//
+//  Created by Scott Leonard on 3/31/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+class Main_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
