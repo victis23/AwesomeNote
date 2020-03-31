bf6e9da57b8444f0a05b086a3bb1dcd371620f47
Michael Wells <wow@duhmarket.com>
2020-03-31 08:41:08 -0400

    Add outlets

--- AwesomeNote/Controller/Main+ViewController.swift
+++ AwesomeNote/Controller/Main+ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class Main_ViewController: UIViewController {
+	
+	@IBOutlet weak var background : UIView!
+	@IBOutlet weak var editButton : UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
