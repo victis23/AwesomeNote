eb2181e554f1bb0cc7f82d1c3bfeeaf6656223bf
Michael Wells <wow@duhmarket.com>
2020-03-30 20:56:52 -0400

    Add deselect method.

--- AwesomeNote/Controller/ViewController+.swift
+++ AwesomeNote/Controller/ViewController+.swift
@@ -74,4 +74,8 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource {
 		
 	}
 	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+	
 }
