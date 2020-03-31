b0aac3ff51181b11148c3ddf85cb1e67a15b8e50
Michael Wells <wow@duhmarket.com>
2020-03-31 09:58:51 -0400

    Add cell for childViewCell

---
+++ AwesomeNote/Views/ChildViewCell+CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChildViewCell+CollectionViewCell.swift
+//  AwesomeNote
+//
+//  Created by Scott Leonard on 3/31/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+class ChildViewCell_CollectionViewCell: UICollectionViewCell {
+    
+}
