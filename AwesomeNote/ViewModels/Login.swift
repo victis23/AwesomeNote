a1bbe42ebbfbe8679501438b05a87869c98eb4fb
Michael Wells <wow@duhmarket.com>
2020-04-01 11:16:27 -0400

    Reposition view

--- AwesomeNote/ViewModels/Login.swift
+++ AwesomeNote/ViewModels/Login.swift
@@ -7,22 +7,42 @@
 //
 
 import SwiftUI
+import Introspect
 
 struct Login: View {
-    var body: some View {
+	
+	@State var isPresenting : Bool = false
+	
+	var body: some View {
 		
 		NavigationView {
 			NavigationLink(destination: MainView()
-				.navigationBarBackButtonHidden(true)
-				, label: {
-				Text("Main")
-			})
-		}.navigationBarTitle("Login")
-    }
+				
+				//SwiftUI shifts the view's position when we remove the navigation controller from the view.
+				//This calculation places it back into its proper position.
+				.position(CGPoint(x: UIScreen.location.maxX / 2, y: UIScreen.location.maxY / 1.687))
+				.edgesIgnoringSafeArea(.all)
+				
+						   ,isActive: $isPresenting) {
+				Button(action: {self.isPresenting.toggle()}) {
+					Text("Access List")
+				}
+			}
+		}.statusBar(hidden: true)
+			.edgesIgnoringSafeArea(.all)
+	}
+}
+	
+	struct Login_Previews: PreviewProvider {
+		static var previews: some View {
+			Login()
+		}
 }
 
-struct Login_Previews: PreviewProvider {
-    static var previews: some View {
-        Login()
-    }
+extension UIScreen {
+	
+	static let screenWidth = UIScreen.main.bounds.size.width
+	static let screenHeight = UIScreen.main.bounds.size.height
+	static let screenSize = UIScreen.main.bounds.size
+	static let location = UIScreen.main.bounds
 }
