c973413ac4bedce55456767dc72c5c62350fa8a6
Michael Wells <wow@duhmarket.com>
2020-04-03 10:21:45 -0400

    Add initializer

--- AwesomeNote/Controller/Helpers/DynamoDBHelper.swift
+++ AwesomeNote/Controller/Helpers/DynamoDBHelper.swift
@@ -14,7 +14,16 @@ import AWSAppSync
 class DynamoDBHelper : Retriever {
 	
 	//AppSync Client used to perform CRUD.
-	private var appSyncClient = (UIApplication.shared.delegate as! AppDelegate).appSyncClientBridge
+	var appSyncClient = (UIApplication.shared.delegate as! AppDelegate).appSyncClientBridge
+	
+	var title : String
+	var content : String
+	
+	init?(title:String, content:String){
+		self.title = title
+		self.content = content
+		super.init(context: (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext)
+	}
 	
 	override public func remove(object:Any){
 		
@@ -22,6 +31,19 @@ class DynamoDBHelper : Retriever {
 	
 	override func saveInDB() {
 		
+		let write = CreateNotesInput(title: title, content: content)
+		
+		appSyncClient?.perform(mutation: CreateNotesMutation(input: write), resultHandler: { (result, error) in
+			
+			if let error = error {
+				print(error.localizedDescription)
+				return
+			}
+			
+			if let _ = result {
+				print("DataBase written successfully.")
+			}
+		})
 	}
 	
 	override func retrieveFromDB(query: NSPredicate? = nil) -> [CDNote]? {
