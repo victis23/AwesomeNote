c061776ab01bdfe3277ec4c6ec8db2652e7e8e54
Michael Wells <wow@duhmarket.com>
2020-04-03 09:35:28 -0400

    Update pod for awsappsync.

---
+++ AwesomeNote/Controller/Helpers/DynamoDBHelper.swift
@@ -0,0 +1,10 @@
+//
+//  DynamoDBHelper.swift
+//  AwesomeNote
+//
+//  Created by Scott Leonard on 4/3/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import Foundation
+import AWSAppSync
